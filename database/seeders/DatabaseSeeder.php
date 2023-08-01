64bf614b88c5ca64b9f182ea6ee713424a5dc219
behrouz mohammadi <dmbehrouz@gmail.com>
2023-08-01 11:21:26 +0330

    TEST GIT

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,6 +38,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             BlogPostSeeder::class,
             CommentsSeeder::class
+            CommentsSeeder::class
         ]);
 
     }
